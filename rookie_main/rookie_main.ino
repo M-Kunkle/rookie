b651a904cf2f2a002d2bba0c8fa5b6dcc51f7bcd
M-Kunkle <101233244+M-Kunkle@users.noreply.github.com>
2022-12-09 18:55:14 -0600

    Update rookie_main.ino

--- rookie_main/rookie_main.ino
+++ rookie_main/rookie_main.ino
@@ -1,84 +1,83 @@
-/* Example sketch to control a stepper motor with TB6600 stepper motor driver and Arduino without a library: continuous rotation. More info: https://www.makerguides.com */
+#include <HIDSerial.h>
+#include <oddebug.h>
+#include <usbconfig-prototype.h>
+#include <usbconfig.h>
+#include <usbdrv.h>
+#include <usbportability.h>
+#include <Matrix.h>
+
 /*
- * Step motor must revolve 6.25 times to go 50 mm (1 space)
- * 
- * 
- * 
- */
 
-// Define stepper motor connections:
-#define dir1 4
-#define step1 5
-#define dir2 2
-#define step2 3
+    ROOKIE - Chess playing robot
+    version 0.0
+    Authors: Matthew Kunkle, Galen Shick
 
-const int stepsPerSpace = 6.25 * 200; // steps * steps per revolution
 
-char but[] = {9,10,11,12};
-int wait = 500;
-String serialData;
-String prevData;
+*/
+
+// Define Arduino Digital Pins (pins unused as of now: 0, 1, a1, a2, a3 )
+// Note: Analog pins can be used as digital
+
+Matrix myMatrix(A4, A5); // To be removed
+
+#define dir1 4        // Stepper 1 DIR+
+#define step1 5       // Stepper 1 PUL+
+#define dir2 6        // Stepper 2 DIR+
+#define step2 8       // Stepper 2 PUL+
+#define killswitch 3  // Interrupt can only be on pin 2 or 3 (pin 2 is USB)
+#define latchPin 12   // Pin 1 on DM74165
+#define clockPin 11   // Pin 2 on DM74165
+#define dataPin 9     // Pin 9 on DM74165
+#define ce 10         // Pin 15 on DM74165
+#define counter93 A0  // Pin 15 on 74LS93
+#define reset93 13    // Pin 2 on 74LS93 -Active HIGH
+#define dplus 2       // USB D+ (Green)
+#define dminus 7      // USB D- (White)
+
+
+const int spaceWidth = 50; // 50mm squares
+const int stepsPerSpace = (spaceWidth / 8) * 200; // revs * steps per rev (1.8 deg/phase, 2mm pitch, 8mm per revolution)
+const int wait = 500; // Delay for stepper steps
 
 void setup() {
 
-  Serial.begin(9600);
-  // Declare pins as output:
+  Serial.begin(9600); // Will swap to new USB
+
+  // PIN DECLARATIONS -----------------------------
   pinMode(step1, OUTPUT);
   pinMode(dir1, OUTPUT);
   pinMode(step2, OUTPUT);
   pinMode(dir2, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, INPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(ce, OUTPUT);
+  pinMode(counter93, OUTPUT);
+  pinMode(reset93, OUTPUT);
+  pinMode(killswitch, INPUT_PULLUP);
+  // ---------------------------------------------
 
-  pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);
+  digitalWrite(reset93, LOW);
+  attachInterrupt(digitalPinToInterrupt(killswitch), kill, CHANGE); // If killswitch is triggered, kill() is called
 
-  for(char i = 0; i<4;i++){
-    pinMode(but[i], INPUT);
-  }
-  
+
+
 }
 
 void loop() {
-  
-  //if(Serial.available()){
-   // wait = Serial.readString().toInt();
-  //}
 
-  wait = analogRead(A0);
-  //Serial.println(wait);
-  int but0 = digitalRead(but[0]);
-  int but1 = digitalRead(but[1]);
-  int but2 = digitalRead(but[2]);
-  int but3 = digitalRead(but[3]);
-
-
-  if(but0){
-    //Serial.print("  Up");
-    Move(wait, 0);
-  } 
-  else if(but1){
-    //Serial.print("  Down");
-    Move(wait, 1);
-  }
-
-  if(but2){
-    //Serial.print("  Left");
-    for (int i = 0; i < stepsPerSpace; i++)
-      Move(wait, 2);
-  }
-  else if(but3){
-    //Serial.print("  Right");
-    Move(wait, 3);
-  }
-  //Serial.println();
-  
-  
 }
+
+
+/*
+   FUNCTION DESCRIPTION HERE
+*/
 
 void moveSpaces(int spaces1, int spaces2, int d1, int d2) {
   digitalWrite(dir1, d1);
   digitalWrite(dir2, d2);
 
-  while(spaces1 > 0 && spaces2 > 0 { // move in both directions at the same time
+  while (spaces1 > 0 && spaces2 > 0 ) { // move in both directions at the same time
     digitalWrite(step1, HIGH);
     digitalWrite(step2, HIGH);
     delayMicroseconds(wait);
@@ -87,13 +86,13 @@ void moveSpaces(int spaces1, int spaces2, int d1, int d2) {
     spaces1--;
     spaces2--;
   }
-  while(spaces1 > 0) { // move the rest of the way in direction 1
+  while (spaces1 > 0) { // move the rest of the way in direction 1
     digitalWrite(step1, HIGH);
     delayMicroseconds(wait);
     digitalWrite(step1, LOW);
     spaces1--;
   }
-  while(spaces2 > 0) { // move the rest of the way in direction 2
+  while (spaces2 > 0) { // move the rest of the way in direction 2
     digitalWrite(step2, HIGH);
     delayMicroseconds(wait);
     digitalWrite(step2, LOW);
@@ -101,16 +100,21 @@ void moveSpaces(int spaces1, int spaces2, int d1, int d2) {
   }
 }
 
-void Move(int wait, int dir){
-  if(wait <=0){
+
+/*
+   FUNCTION DESCRIPTION HERE
+*/
+
+void Move(int wait, int dir) {
+  if (wait <= 0) {
     return;
   }
 
-  if(dir < 0 or dir >= 4){
+  if (dir < 0 or dir >= 4) {
     return;
   }
 
-  if (dir == 0){
+  if (dir == 0) {
     // UP
     digitalWrite(dir1, HIGH);
     digitalWrite(step1, HIGH);
@@ -118,7 +122,7 @@ void Move(int wait, int dir){
     digitalWrite(step1, LOW);
     delayMicroseconds(wait);
   }
-  else if(dir == 1){
+  else if (dir == 1) {
     // DOWN
     digitalWrite(dir1, LOW);
     digitalWrite(step1, HIGH);
@@ -126,7 +130,7 @@ void Move(int wait, int dir){
     digitalWrite(step1, LOW);
     delayMicroseconds(wait);
   }
-  else if(dir == 2){
+  else if (dir == 2) {
     // RIGHT
     digitalWrite(dir2, HIGH);
     digitalWrite(step2, HIGH);
@@ -134,7 +138,7 @@ void Move(int wait, int dir){
     digitalWrite(step2, LOW);
     delayMicroseconds(wait);
   }
-  else if(dir == 3){
+  else if (dir == 3) {
     // LEFT
     digitalWrite(dir2, LOW);
     digitalWrite(step2, HIGH);
@@ -142,4 +146,68 @@ void Move(int wait, int dir){
     digitalWrite(step2, LOW);
     delayMicroseconds(wait);
   }
+}
+
+
+/*
+   FUNCTION DESCRIPTION HERE
+*/
+
+void kill(void) {
+  for (;;);
+}
+
+/*
+  FUNCTION DESCRIPTION HERE
+*/
+
+void readBoard() {
+  byte bus;
+  for (int j = 0; j < 8; j++){
+    digitalWrite(clockPin, HIGH); //First step here will be to pulse the latch/reset pin while sending a falling clock signal.
+    digitalWrite(ce, HIGH);
+    digitalWrite(latchPin, LOW); //This will lock in the current state of the inputs to be sent to the arduino.
+    delayMicroseconds(5);
+    digitalWrite(clockPin, LOW);
+    digitalWrite(ce, LOW);
+    digitalWrite(latchPin, HIGH);
+  
+    for (int i = 0; i < 8; i++) {
+      bitWrite(bus, i, digitalRead(dataPin)); //Grab our byte, select which bit, and write the current input as 1 or 0
+  
+      digitalWrite(clockPin, LOW);
+      delayMicroseconds(5);
+      digitalWrite(clockPin, HIGH); //send a clock leading edge so to load the next bit
+    } 
+    
+  }
+  printBoard();
+}
+
+/*
+  FUNCTION DESCRIPTION HERE
+*/
+
+void printBoard() {
+  char output[100];
+
+  for (byte i = 0; i < 8; i++) {
+    sprintf(output, "%d: %02x\n", i, board[i]);
+    Serial.println(output);
+  }
+
+  // New Matrix Stuff
+  uint8_t  LEDArray[8];
+  myMatrix.clear();
+  for (int i = 0; i < 8; i++)
+  {
+    LEDArray[i] = board[i];
+    for (int j = 7; j >= 0; j--)
+    {
+      if ((LEDArray[i] & 0x01) == 1)
+        myMatrix.drawPixel(j, i, 1);
+      LEDArray[i] = LEDArray[i] >> 1;
+    }
+  }
+  myMatrix.writeDisplay();
 }
