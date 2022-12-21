08955c1a85fd2885fe98c7b34cadf0853cade739
M-Kunkle <101233244+M-Kunkle@users.noreply.github.com>
2022-12-21 02:09:29 -0600

    added fen parsing

--- chess_engine.c
+++ chess_engine.c
@@ -14,9 +14,19 @@
 #define pop_bit(bitboard, square) ((bitboard) &= ~(1ULL << (square)))
 
 // ------------------------------------------
+// FEN debug strings
+// ------------------------------------------
+#define empty_board "8/8/8/8/8/8/8/8 w - - "
+#define start_position "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 "
+#define tricky_position "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1 "
+#define killer_position "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
+#define cmk_position "r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9 "
+
+// ------------------------------------------
 // function declarations
 // ------------------------------------------
 void printbitboard(U64 bitboard);
+void printboard(void);
 U64 mask_pawn_attacks(int side, int square);
 U64 mask_knight_attacks(int square);
 U64 mask_king_attacks(int square);
@@ -93,7 +103,7 @@ int char_pieces[] = {
 };
 
 // side colors
-enum { white, black };
+enum { white, black, both };
 
 // qol for bishop/rook attacks
 enum { rook, bishop };
@@ -237,11 +247,104 @@ void printboard(){
   }
   // print file layout
   printf("\n      a   b   c   d   e   f   g   h\n");
-  printf("\nSIDE TO MOVE: %s", !side ? "White" : "black");
-  printf("\nAVAILABLE ENPASSANT: %s", enpassant != no_sq ? square_to_coord[enpassant] : "N/A");
+  printf("\nSIDE TO MOVE:          %s", !side ? "White" : "Black");
+  printf("\nAVAILABLE ENPASSANT:   %s", enpassant != no_sq ? square_to_coord[enpassant] : "N/A");
   printf("\nWHITE CASTLING RIGHTS: %s, %s", (castle & wk) ? "Kingside" : "--------", (castle & wq) ? "Queenside" : "--------");
   printf("\nBLACK CASTLING RIGHTS: %s, %s", (castle & bk) ? "Kingside" : "--------", (castle & bq) ? "Queenside" : "--------");
   printf("\n\n");
+}
+
+// FEN Parsing
+void parse_fen(char *fen){
+  
+  // reset bitboards and state
+  memset(bitboards, 0ULL, sizeof(bitboards));
+  memset(occupancies, 0ULL, sizeof(occupancies));
+  side = 0;
+  enpassant = no_sq;
+  castle = 0;
+
+  // loop over board to get board state
+  for(int rank = 0; rank < 8; rank++){
+    for(int file = 0; file < 8; file++){
+      int square = rank * 8 + file;
+      
+      if((*fen >= 'a' && *fen <= 'z') || (*fen >= 'A' && *fen < 'Z')){
+        int piece = char_pieces[*fen];
+        set_bit(bitboards[piece], square);
+        *fen++;
+      }
+
+      if (*fen >= '0' && *fen <= '9'){
+        // little trick here to convert char 0 to int 0
+        int offset = *fen - '0';
+        int piece = -1;
+
+        // loop over piece bitboards to find what piece resides on this square
+        for (int bb_idx = P; bb_idx <= k; bb_idx++){
+          if(get_bit((bitboards[bb_idx]), square)){
+            piece = bb_idx;
+          }
+        }
+
+        if(piece == -1){
+          file--;
+        }
+
+        file += offset;
+        *fen++;
+        }
+
+      if(*fen == '/'){
+        *fen++;
+      }
+    }
+  }
+
+  *fen++;
+
+  // parse side to move
+  (*fen == 'w') ? (side = white) : (side = black);
+
+  // parse castling rights
+  fen += 2; 
+  while(*fen != ' '){
+    switch(*fen){
+      case 'K': castle |= wk; break;
+      case 'Q': castle |= wq; break;
+      case 'k': castle |= bk; break;
+      case 'q': castle |= bq; break;
+    }
+    *fen++;
+  }
+
+  // parse enpassant square
+  *fen++;
+  if(*fen != '-'){
+    int enpass_file = fen[0] - 'a';
+    int enpass_rank = 8 - (fen[1] - '0');
+    enpassant = enpass_rank * 8 + enpass_file;
+  } else {
+    enpassant = no_sq;
+  }
+
+  // initialization of occupancy bitboards
+
+  // white occupancy bitboards
+  for(int piece = P; piece <= K; piece++){
+    occupancies[white] |= bitboards[piece];
+  }
+
+  // black occupancy bitboards
+  for(int piece = p; piece <= k; piece++){
+    occupancies[black] |= bitboards[piece];
+  }
+
+  occupancies[both] = occupancies[white] | occupancies[black];
+
+
+
+  printf("fen: %s\n", fen);
 }
 
 // ----------------------------------------
@@ -652,7 +755,6 @@ U64 get_rook_attacks(int square, U64 occupancy){
   return rook_attacks[square][occupancy];
 }
 
-
 // set occupied squares
 U64 set_occup(int index, int bits_in_mask, U64 attack_mask){
   U64 map = 0ULL;
@@ -724,7 +826,6 @@ void init_magic_nums(){
 // intializations
 // ----------------------------------------
 
-
 void init_leapers_attacks(){
   for (int square=0; square<64; square++){
     pawn_attacks[white][square] = mask_pawn_attacks(white, square);
@@ -771,61 +872,12 @@ void init_all(){
 
 int main(){
   init_all();
-      // set white pawns
-    set_bit(bitboards[P], a2);
-    set_bit(bitboards[P], b2);
-    set_bit(bitboards[P], c2);
-    set_bit(bitboards[P], d2);
-    set_bit(bitboards[P], e2);
-    set_bit(bitboards[P], f2);
-    set_bit(bitboards[P], g2);
-    set_bit(bitboards[P], h2);
-    
-    // set white knights
-    set_bit(bitboards[N], b1);
-    set_bit(bitboards[N], g1);
-    
-    // set white bishops
-    set_bit(bitboards[B], c1);
-    set_bit(bitboards[B], f1);
-    
-    // set white rooks
-    set_bit(bitboards[R], a1);
-    set_bit(bitboards[R], h1);
-    
-    // set white queen & king
-    set_bit(bitboards[Q], d1);
-    set_bit(bitboards[K], e1);
-    
-    // set white pawns
-    set_bit(bitboards[p], a7);
-    set_bit(bitboards[p], b7);
-    set_bit(bitboards[p], c7);
-    set_bit(bitboards[p], d7);
-    set_bit(bitboards[p], e7);
-    set_bit(bitboards[p], f7);
-    set_bit(bitboards[p], g7);
-    set_bit(bitboards[p], h7);
-    
-    // set white knights
-    set_bit(bitboards[n], b8);
-    set_bit(bitboards[n], g8);
-    
-    // set white bishops
-    set_bit(bitboards[b], c8);
-    set_bit(bitboards[b], f8);
-    
-    // set white rooks
-    set_bit(bitboards[r], a8);
-    set_bit(bitboards[r], h8);
-    
-    // set white queen & king
-    set_bit(bitboards[q], d8);
-    set_bit(bitboards[k], e8);
 
-    side = white;
-    castle = 0b0110;
-    printboard();
+  side = black;
+  castle = 0b0110;
+
+  parse_fen(tricky_position);
+  printboard();
 
   return 0;
 }
